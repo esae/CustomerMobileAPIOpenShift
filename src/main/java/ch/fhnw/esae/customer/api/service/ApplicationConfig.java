fdb4825c5e9ae6a4f9f428e2b19581f6b5f0a842
Andreas Martin <andreas.martin@andreasmartin.ch>
2016-12-02 16:57:34 +0100

    CORS update

--- src/main/java/ch/fhnw/esae/customer/api/service/ApplicationConfig.java
+++ src/main/java/ch/fhnw/esae/customer/api/service/ApplicationConfig.java
@@ -29,6 +29,7 @@ public class ApplicationConfig extends Application {
      * If required, comment out calling this method in getClasses().
      */
     private void addRestResourceClasses(Set<Class<?>> resources) {
+        resources.add(ch.fhnw.esae.customer.api.service.CORSFilter.class);
         resources.add(ch.fhnw.esae.customer.api.service.CustomerEndpoint.class);
     }
     
---
+++ src/main/java/ch/fhnw/esae/customer/api/service/CORSFilter.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ch.fhnw.esae.customer.api.service;
+
+import java.io.IOException;
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.ext.Provider;
+
+/**
+ *
+ * @author andreas.martin
+ */
+@Provider
+public class CORSFilter implements ContainerResponseFilter{
+
+    @Override
+    public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) throws IOException {
+        responseContext.getHeaders().add("Access-Control-Allow-Origin", "*");
+        responseContext.getHeaders().add("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+        String requesteader = requestContext.getHeaderString("Access-Control-Request-Headers");
+        if (requesteader != null && !"".equals(requesteader)) {
+            responseContext.getHeaders().putSingle("Access-Control-Allow-Headers", requesteader);
+        }
+        responseContext.getHeaders().add("Access-Control-Max-Age", "86400");
+    }
+
+}
+    
